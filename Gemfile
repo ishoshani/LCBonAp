7b89ceb1b288b270fa8cec3e0ea0f3c4e710d8bf
Ido Shoshani <idoshoshani@lclark.edu>
2015-03-02 10:59:09 -0800

    webscraping

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 ruby "2.1.1"
 source 'https://rubygems.org'
+gem 'nokogiri', '>= 2.92'
 gem 'sinatra', '>= 1.4.5'
 gem 'sqlite3', '>= 1.3.9'
 gem 'shotgun', '>= 0.9'
--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 # Logic for BonApp
 require 'rubygems'
 require 'bundler/setup'
+require 'open-uri'
 Bundler.require
 
 get '/' do
 	erb :index
+	url='http://lewisandclark.cafebonappetit.com/'
+	data=Nokogiri::HTML(open(url))
+	meals=data.css('daypart-menu')
 end
 
