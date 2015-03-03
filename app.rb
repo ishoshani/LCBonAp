5e39ce2b2bab52292925af93802ed98d086bb07e
Connor Lay <connor.lay@me.com>
2015-03-02 19:16:58 -0800

    Index now displays menu items in a basic table.

--- app.rb
+++ app.rb
@@ -1,9 +1,19 @@
 # Logic for BonApp
 require 'rubygems'
 require 'bundler/setup'
+require 'open-uri'
+require 'nokogiri'
 Bundler.require
 
+MENU_URL = "http://legacy.cafebonappetit.com/print-menu/cafe/150/menu/74066/days/today/"
+
+def scrape_bon
+  html = Nokogiri::HTML(open(MENU_URL))
+  @items = html.css(".station strong").zip(html.css(".description strong"))
+end
+
 get '/' do
+  scrape_bon
 	erb :index
 end
 
