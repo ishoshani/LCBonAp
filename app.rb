f319093a4fb5edee239c7b39fbe4eea0cb0457bc
Connor Lay <connor.lay@me.com>
2015-03-09 14:42:51 -0700

    Fixed bug for displaying Sundays.

--- app.rb
+++ app.rb
@@ -10,14 +10,18 @@ MENU_URL = "http://legacy.cafebonappetit.com/print-menu/cafe/150/menu/"
 
 def scrape_menu
   html = Nokogiri::HTML(open(MENU_URL))
-  today = html.css("#menu-items .eni-menu-day-#{Date.today.cwday}")
+  if Date.today.cwday == 7 # Fix for sundays.
+    @today = html.css("#menu-items .eni-menu-day-0")
+  else
+    @today = html.css("#menu-items .eni-menu-day-#{Date.today.cwday}")
+  end
   @meals = Hash.new
 
-  today.css(".always-show-me td strong").each do |meal|
+  @today.css(".always-show-me td strong").each do |meal|
     @meals[meal.text] = Array.new
   end
 
-  today.css("table tr").each do |element|
+  @today.css("table tr").each do |element|
     if (element["class"] == "always-show-me")
       @meal = element.css("td strong").text
     else
